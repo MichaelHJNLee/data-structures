294fe42880f3fca9d397dfece2f51366fd0c28a7
Michael Lee <michaellee@Michaels-MacBook-Pro.local>
2019-07-17 14:31:07 -0700

    Completed functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -12,23 +12,23 @@ var queueMethods = {};
 queueMethods.enqueue = function (value){
 	var last = 0;
 	for (var key in this.storage){
-		if (key > last){
-			last = key;
+		if (parseInt(key) > last){
+			last = parseInt(key);
 		}
 	}
 	this.storage[last + 1] = value;
 }
 
 queueMethods.dequeue = function(){
-	var first = this.storage['1'];
-	delete this.storage['1'];
 	for (var key in this.storage){
-		var newKey = key - 1;
+		var newKey = parseInt(key) - 1;
 		var val = this.storage[key];
-		delete this.storage[key];
 		this.storage[newKey] = val;
+		delete this.storage[key];
 	}
-	return first;
+	var deleted = this.storage[0];
+	delete this.storage[0];
+	return deleted;
 }
 
 queueMethods.size = function(){
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,42 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+  _.extend(newStack, stackMethods);
+  return newStack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+	var last = 0;
+	for (var key in this.storage){
+		if (parseInt(key) > last){
+			last = parseInt(key);
+		}
+	}
+	this.storage[last + 1] = value;
+}
+
+stackMethods.pop = function(){
+	var last = 0;
+	for (var key in this.storage){
+		if (parseInt(key) > last){
+			last = parseInt(key);
+		}
+	}
+	var deleted = this.storage[last];
+	delete this.storage[last];
+	return deleted;
+}
+
+stackMethods.size = function(){
+	var counter = 0;
+	for (var key in this.storage){
+		counter++;
+	}
+	return counter;
+}
 
 
