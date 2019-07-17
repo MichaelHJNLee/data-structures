28602fb62508e46e5f6305a0783122ca91317b7b
Michael Lee <michaellee@Michaels-MacBook-Pro.local>
2019-07-17 12:16:01 -0700

    Passed all tests for functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,33 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var last = 0;
+    for (var key in storage){
+      if (parseInt(key) > last){
+        last = parseInt(key);
+      }
+    }
+    storage[last + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    var deleted = storage['1'];
+    delete storage['1'];
+    for (var key in storage){
+      var val = storage[key];
+      var newKey = parseInt(key) - 1;
+      delete storage[key];
+      storage[newKey] = val;
+    }
+    return deleted;
   };
 
   someInstance.size = function() {
+    var counter = 0;
+    for (var key in storage){
+      counter++;
+    }
+    return counter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,33 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var last = 0;
+    for (var key in storage){
+      if (parseInt(key) > last){
+        last = parseInt(key);
+      }
+    }
+    storage[last + 1] = value;
   };
 
   someInstance.pop = function() {
+    var last = 0;
+    for (var key in storage){
+      if (parseInt(key) > last){
+        last = parseInt(key);
+      }
+    }
+    var deleted = storage[last];
+    delete storage[last];
+    return deleted;
   };
 
   someInstance.size = function() {
+    var counter = 0;
+    for (var key in storage){
+      counter++;
+    }
+    return counter;
   };
 
   return someInstance;
